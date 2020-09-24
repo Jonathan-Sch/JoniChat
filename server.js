889f5be8e66866ec95a4cb20f6ff2c67a49eaf65
Jonathan Scheil <jonathan_scheil@web.de>
2020-09-24 21:46:06 +0200

    Frontend work, userlist

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ var userData = [{
 		
 		{"name":"Testperson2",
 		"chats":[{"chatpartner":"Michael", "messages":[{"message1": "First Text"}, {"message2":"Second Text"}]},
-			{"chatpartner":"Ludolf", "messages":[{"message1": "First Lorem Ipsum dolor"}, {"message2":"Second Lorem Ipsum dolor"}]}]
+			{"chatpartner":"Ludolf", "messages":[{"message1": "First Lorem Ipsum dolor"}, {"message2":"Second Lorem Ipsum dolor"}]},
+			{"chatpartner":"Elisa", "messages":[{"message1": "First Testperson2 text"}]}]
 	
 	
 }]
@@ -23,16 +24,32 @@ var userData = [{
 
 app.listen(3000);
 
-app.get("/user/:name", function(req, res){
+app.get("/user/:name/:searchname", function(req, res){
 	var username = req.params.name;
-	var users = showUsers(username);
+	var searchname = req.params.searchname;
+	var selectedusers = showMessages(username, searchname);
 res.contentType = "application/json";
-res.status(200).send(users);
+res.status(200).send(selectedusers);
 
 });
 
+app.get("/user/:name", function(req, res){
+	var currentUser = req.params.name;
+	var messagedUsers = showContactedUsers(currentUser);
+	res.contentType = "application/json";
+	res.status(200).send(messagedUsers);
+})
 
-function showUsers(username){
+app.get("/user", function(req,res){
+	
+	var users = showAllUsers();
+	res.contentType="application/json";
+	res.status(200).send(users);
+	
+})
+
+
+function showMessages(username, searchname){
 	
 	console.log(username);
 	
@@ -40,12 +57,43 @@ function showUsers(username){
 	
 	for(var i = 0; i < userData.length; i++){
 		console.log(userData[i].name);
-		for(var j = 0; j <userData[i].chats.length; j++){
-			console.log(userData[i].chats[j].chatpartner);
-			if(userData[i].chats[j].chatpartner == username){
-				contactedUsers.push(userData[i].chats[j].messages);
-			}
+		if(userData[i].name == username){
+			for(var j = 0; j <userData[i].chats.length; j++){
+				console.log(userData[i].chats[j].chatpartner);
+				if(userData[i].chats[j].chatpartner == searchname){
+					contactedUsers.push(userData[i].chats[j].messages);
+				}
+		}
+		
 		}
 	}
 	return contactedUsers;
+}
+
+
+function showContactedUsers(username){
+	var messagedUsers = [];
+	for(var i = 0; i < userData.length; i++){
+		if(userData[i].name === username){
+			for(var j = 0; j < userData[i].chats.length; j++){
+				messagedUsers.push(userData[i].chats[j].chatpartner);
+			}
+		}
+	}
+	return messagedUsers;
+}
+
+function showAllUsers(){
+	
+var Users = [];
+	
+	for(var i = 0; i < userData.length; i++){
+		console.log(userData[i].name);
+		
+			
+				Users.push(userData[i].name);
+			
+		
+	}
+	return Users;
 }
